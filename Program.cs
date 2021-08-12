dceed46e85110b1e97cb557ebc4db08d8f4f61dc
Anton Georgesco <antongeorgescu@gmail.com>
2021-08-12 16:47:26 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAAlgorithm2
+{
+    class Program
+    {
+        static UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        static RSACryptoServiceProvider RSA = null;
+        
+        static void Main(string[] args)
+        {
+            // enter string to encrypt
+            Console.WriteLine("Enter the string to encrypt...");
+            var textToEncrypt = Console.ReadLine();
+            Console.WriteLine($"String to encrypt:{textToEncrypt}");
+
+            // generate key pair and store in 2 different files
+            Console.WriteLine("Generate key pair and store in 2 different files.");
+            RSAKeyManagement.GenerateRsaKeyPair("privateKey.txt", "publicKey.txt");
+
+            // load public key from its file and apply encryption
+            Console.WriteLine("Load public key from its file and apply encryption.");
+            RSA = RSAKeyManagement.PublicKeyFromPemFile("publicKey.txt");
+            byte[] plaintext = ByteConverter.GetBytes(textToEncrypt);
+            byte[] encryptedtext = RSAKeyManagement.Encryption(plaintext, RSA.ExportParameters(false), false);
+
+            // load private key from its file and apply decryption
+            Console.WriteLine("Load private key from its file and apply decryption.");
+            RSA = RSAKeyManagement.PrivateKeyFromPemFile("privateKey.txt");
+            byte[] decryptedtex = RSAKeyManagement.Decryption(encryptedtext,
+            RSA.ExportParameters(true), false);
+            var decryptedText = ByteConverter.GetString(decryptedtex);
+
+            // display the decrypted string
+            Console.WriteLine($"Decrypted string:{decryptedText}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RSAKeyManagement.cs
@@ -0,0 +1,130 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAAlgorithm2
+{
+    public static class RSAKeyManagement
+    {
+        public static void GenerateRsaKeyPair(String privateKeyFilePath, String publicKeyFilePath)
+        {
+            RsaKeyPairGenerator rsaGenerator = new RsaKeyPairGenerator();
+            rsaGenerator.Init(new KeyGenerationParameters(new SecureRandom(), 2048));
+            var keyPair = rsaGenerator.GenerateKeyPair();
+
+
+            using (TextWriter privateKeyTextWriter = new StringWriter())
+            {
+
+                PemWriter pemWriter = new PemWriter(privateKeyTextWriter);
+                pemWriter.WriteObject(keyPair.Private);
+                pemWriter.Writer.Flush();
+                File.WriteAllText(privateKeyFilePath, privateKeyTextWriter.ToString());
+            }
+
+
+            using (TextWriter publicKeyTextWriter = new StringWriter())
+            {
+
+                PemWriter pemWriter = new PemWriter(publicKeyTextWriter);
+                pemWriter.WriteObject(keyPair.Public);
+                pemWriter.Writer.Flush();
+
+                File.WriteAllText(publicKeyFilePath, publicKeyTextWriter.ToString());
+            }
+        }
+
+        public static RSACryptoServiceProvider PrivateKeyFromPemFile(String filePath)
+        {
+            using (TextReader privateKeyTextReader = new StringReader(File.ReadAllText(filePath)))
+            {
+                AsymmetricCipherKeyPair readKeyPair = (AsymmetricCipherKeyPair)new PemReader(privateKeyTextReader).ReadObject();
+
+
+                RsaPrivateCrtKeyParameters privateKeyParams = ((RsaPrivateCrtKeyParameters)readKeyPair.Private);
+                RSACryptoServiceProvider cryptoServiceProvider = new RSACryptoServiceProvider();
+                RSAParameters parms = new RSAParameters();
+
+                parms.Modulus = privateKeyParams.Modulus.ToByteArrayUnsigned();
+                parms.P = privateKeyParams.P.ToByteArrayUnsigned();
+                parms.Q = privateKeyParams.Q.ToByteArrayUnsigned();
+                parms.DP = privateKeyParams.DP.ToByteArrayUnsigned();
+                parms.DQ = privateKeyParams.DQ.ToByteArrayUnsigned();
+                parms.InverseQ = privateKeyParams.QInv.ToByteArrayUnsigned();
+                parms.D = privateKeyParams.Exponent.ToByteArrayUnsigned();
+                parms.Exponent = privateKeyParams.PublicExponent.ToByteArrayUnsigned();
+
+                cryptoServiceProvider.ImportParameters(parms);
+
+                return cryptoServiceProvider;
+            }
+        }
+
+        public static RSACryptoServiceProvider PublicKeyFromPemFile(String filePath)
+        {
+            using (TextReader publicKeyTextReader = new StringReader(File.ReadAllText(filePath)))
+            {
+                RsaKeyParameters publicKeyParam = (RsaKeyParameters)new PemReader(publicKeyTextReader).ReadObject();
+
+                RSACryptoServiceProvider cryptoServiceProvider = new RSACryptoServiceProvider();
+                RSAParameters parms = new RSAParameters();
+
+
+
+                parms.Modulus = publicKeyParam.Modulus.ToByteArrayUnsigned();
+                parms.Exponent = publicKeyParam.Exponent.ToByteArrayUnsigned();
+
+
+                cryptoServiceProvider.ImportParameters(parms);
+
+                return cryptoServiceProvider;
+            }
+        }
+
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+    }
+}
